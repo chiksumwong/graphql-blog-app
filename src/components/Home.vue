77e8ba4d842bd6dbae445a4c6e13b81bda335464
CHIK SUM <chiksum.wong@gmail.com>
2018-06-16 21:15:53 +0800

    Added Page "Home"

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,36 @@
 <template>
-    <div>
-        <h1>Welcome Home</h1>
-    </div>
+    <section class="section">
+        <div class="columns">
+            <div class="column is-6 is-offset-3">
+                <h1 class="title">New Article</h1>
+
+                <h3 v-for="post in allPosts"
+                        :key="post.id"
+                        class="title is-5">
+                    <router-link :to="`/posts/${post.id}`">
+                        {{ post.title }}
+                    </router-link>
+                </h3>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
-    export default {
-        
+import { ALL_POSTS_QUERY } from '@/graphql'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      allPosts: []
     }
+  },
+  apollo: {
+    // fetch all posts
+    allPosts: {
+      query: ALL_POSTS_QUERY
+    }
+  }
+}
 </script>
-
-<style scoped>
-
-</style>
