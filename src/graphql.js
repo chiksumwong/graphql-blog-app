a64ba919989611d6d4a70526077f05712dcca88f
CHIK SUM <chiksum.wong@gmail.com>
2018-06-16 15:18:32 +0800

    create graphql global file

---
+++ src/graphql.js
@@ -0,0 +1,18 @@
+/* 
+   用於處理所有GraphQL查詢和變更的全局文件
+ */
+import gql from 'graphql-tag'
+
+export const SIGNUP_MUTATION = gql `
+    mutation SignupMutation($name: String!, $email: String!, $password: String!) {
+        createUser(
+            name: $name,
+            email: $email,
+            password: $password
+        ) {
+            id
+            name
+            email
+        }
+    }
+`
