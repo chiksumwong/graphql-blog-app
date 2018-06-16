507e20958dd0caeb0b2f98c64d2b3fe2b44b286f
CHIK SUM <chiksum.wong@gmail.com>
2018-06-16 22:41:31 +0800

    Update Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,59 @@
 <template>
-    <div id="app">
-        <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
-            <div class="container">
-                <div class="navbar-brand">
-                    <router-link class="navbar-item" to="/">GraphQL Blog</router-link>
+  <div id="app">
 
-                    <button class="button navbar-burger">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </button>
-                </div>
+    <nav class="navbar is-dark">
+
+      <div class="navbar-brand">
+
+        <router-link class="navbar-item" to="/">GraphQL Blog</router-link>
+
+        <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+      </div>
+
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <router-link class="navbar-item" to="/">Articles</router-link>
+        </div>
+
+        <!-- Navbar-Right -->
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="field is-grouped">
+
+              <p class="control">
+                <router-link class="button is-primary" to="/logIn">LOGIN</router-link>
+              </p>
+              <p class="control">
+                <router-link class="button is-primary" to="/signup">REGISTRATION</router-link>
+              </p>
+
             </div>
-        </nav>
-        <router-view/>
-    </div>
+          </div>
+        </div>
+        <!-- End Navbar-Right -->
+
+
+      </div>
+    </nav>
+    <!-- End Navbar -->
+
+    <router-view/>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App'
+  }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
