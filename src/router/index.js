7189f73dc0ef1b7a3f983a6789bc6e9fd99d45ef
CHIK SUM <chiksum.wong@gmail.com>
2018-06-16 17:15:38 +0800

    Add GraphQL - Login Function
    Added posts page

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
+
+import Home from '@/components/Home'
+
 import SignUp from '@/components/Admin/SignUp'
+import LogIn from '@/components/Admin/LogIn'
+
+import Posts from '@/components/View/Posts'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
       path: '/signup',
       name: 'SignUp',
       component: SignUp
-    }
+    },
+    {
+      path: '/login',
+      name: 'LogIn',
+      component: LogIn
+    },
+    {
+      path: '/admin/posts',
+      name: 'Posts',
+      component: Posts
+    },
   ]
 })
