1c8dfb85868a8cfd227d11840a66d4d98168efa7
CHIK SUM <chiksum.wong@gmail.com>
2018-06-16 15:02:23 +0800

    Basic setting

--- src/main.js
+++ src/main.js
@@ -4,12 +4,42 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+/*
+*   Apollo相關擴展包實現與GraphQL服務端的交互
+*/
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+
+
 Vue.config.productionTip = false
+
+
+const httpLink = new HttpLink({
+  // URL to graphql server, you should use an absolute URL here
+  uri: 'http://apidemo.test/graphql'
+})
+
+// create apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+// install vue plugin
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  apolloProvider,
   components: { App },
   template: '<App/>'
 })
